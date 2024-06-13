0ff66a15b48d616bd5186201b8d65127decded07
Animesh Kumar <animeshkumar@Animeshs-MacBook-Air.local>
2024-06-13 20:43:42 +0530

    Add animation to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,30 @@
+import { useRef, useEffect } from "react";
 import { FaLinkedinIn, FaGithub, FaRegCopyright } from "react-icons/fa";
+import { motion, useInView, useAnimation } from "framer-motion";
 import Resume from "../assets/files/Resume.pdf";
 import Image from "../assets/images/footer_image.jpeg"
 import "../styles/Footer.css"
 function Footer () {
+    const ref = useRef(null);
+    const isInView = useInView(ref, { once: true });
+    const mainControls = useAnimation();
+    useEffect(() => {
+        if (isInView) {
+        mainControls.start("visible");
+        }
+    }, [isInView]);
     return (
-        <div className="footer-container">
-            <div className="child-container left-div">
+        <motion.div className="footer-container">
+            <motion.div className="child-container left-div"
+            ref={ref}
+            variants={{
+                hidden: { opacity: 0, x: -75 },
+                visible: { opacity: 1, x: 0 },
+              }}
+              initial="hidden"
+              animate={mainControls}
+              transition={{ duration: 0.5, delay: 0.25 }}
+            >
                 <div className="text-container">
                     <span className="left-div-span">Let's Talk</span>
                     <br/>
@@ -28,11 +47,19 @@ function Footer () {
                     <p><FaRegCopyright /> 2024 by Animesh Kumar</p>
                     
                 </div>
-            </div>
-            <div className="child-container right-div">
+            </motion.div>
+            <motion.div className="child-container right-div"
+            variants={{
+                hidden: { opacity: 0, x: 75 },
+                visible: { opacity: 1, x: 0 },
+              }}
+              initial="hidden"
+              animate={mainControls}
+              transition={{ duration: 0.5, delay: 0.35 }}
+            >
                 <img src={Image}/>
-            </div>
-        </div>
+            </motion.div>
+        </motion.div>
     )
 }
 
